d1dc74d0a92789224c983e813089cc77c39e5f50
Carla H <hetterichcarla@gmail.com>
2024-06-27 13:18:42 +0200

    descrição das imagens

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,12 +1,11 @@
 // Modo noturno
-const inputCheck = document.querySelector('#modo-noturno')
-const elemento = document.querySelector('body')
+const inputCheck = document.querySelector('#modo-noturno');
+const elemento = document.querySelector('body');
 
 inputCheck.addEventListener('click', () => {
     const modo = inputCheck.checked ? 'light' : 'dark'
     elemento.setAttribute('data-bs-theme', modo)
-})
-
+});
 
 
 // Script para inicialização do carrossel
